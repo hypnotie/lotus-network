d9d8a3addf29f0b99b2165914b983cb3d996d01d
hypnotie <maksymhanhalo@gmail.com>
2022-04-23 03:26:55 +0300

    add files

--- src/components/FormValidation/LoginFormSchema.js
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-const loginFormSchema = yup.object().shape({
-	email: yup.string().email("Enter proper e-mail").required("Required field"),
-	password: yup.string().typeError("Should be a string").required("fsdf")
-});
-
-export default loginFormSchema;
--- src/components/common/Preloader/Preloader.js
+++
@@ -1,7 +0,0 @@
-import preloader from "../../../img/icons/users/preloader.svg";
-
-let Preloader = () => {
-	return <img src={preloader} alt="Loading..." />
-};
-
-export default Preloader;
